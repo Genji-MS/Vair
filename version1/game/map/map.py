e58958e05c449d9570cd8213b47086d332c535ca
JohnminerIv <john.douglas.miner.iv@gmail.com>
2021-03-15 12:55:29 -0800

    setting up venv and docs as well as adding in initial thoughs for map generation.

--- version1/game/map/map.py
+++ version1/game/map/map.py
@@ -26,5 +26,25 @@ class Location:
 
 
 class Chunk:
+    """
+    There are two ways I can think of to generate the landscape, one would be to
+    generate the land based on surounding tiles so like if we have tiles:
+    [barren, barren, prarry] surounding the tile we are genrating we woul have:
+    barren = [0.5, 0.5, 0] * 2
+    prarry = [0.25, 0.5, 0.25]
+    or [1.25, 1.5, 0.25] = 
+       [0.4166, 0.5, 0.0833] probabilities of getting a
+       [barren, plain, lush] tile
+
+    , thus making it reliant on existing
+    tiles but more easily extended if we wanted a continous open world feel.
+    The way to do it would be to generate a smooth depth map sort of thing using
+    numpy and then using the depth to determine what the tile is, this could make
+    the generation time faster but might make it difficulte to make the different
+    sections of the map's bioms line up when crossing "chunk" borders.
+
+    """
+
     def __init__(self, height=100, width=100):
         self.arrays = []
+        ...
