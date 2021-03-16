38c59e9df122c3af9a58067f943f5790dc35c1dd
xScoopy <JLeary070@gmail.com>
2021-03-15 20:37:25 -0500

    Completed functionality to play intro song located in intro_music.py.

---
+++ version1/game/intro_music.py
@@ -0,0 +1,19 @@
+import pyglet as py
+#set up resource path
+# py.resource.path = ['./resources']
+# py.resource.reindex()
+
+#set audio driver options
+py.options['audio'] = ('openal', 'pulse', 'directsound', 'silent')
+
+#reference intro song file
+intro = py.media.load("./resources/intro.wav")
+
+#play intro
+intro.play()
+
+py.app.run()
+
+#works as is if run directly from this file
+
+
