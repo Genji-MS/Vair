f251233dc9a824542ef58c65dc8be15429789f5b
JohnminerIv <john.douglas.miner.iv@gmail.com>
2021-03-17 11:02:52 -0800

    food generates on locations now

--- version1/map/map.py
+++ version1/map/map.py
@@ -4,23 +4,64 @@ import random
 import time
 import os
 import json
+from ..game.food import Food
 
-from numpy import tile
+
+class Player:
+    def __init__(self):
+        self.pos = (15, 5)
+        self.value = '\u001b[31mX'
+
+
+class GameMap:
+    def __init__(self) -> None:
+        self.chunks = []
+        self.player = Player()
+        self.current_chunk = 0
+        chunk = Chunk(0, '0')
+        chunk.map[self.player.pos[0]][self.player.pos[1]
+                                      ].colliding_objects = [self.player]
+        print(chunk)
+        self.chunks.append(chunk)
+        for i in range(1, 10):
+            chunk = Chunk(i, str(i), passed_map=chunk.slice(
+                (0, 19, 1, 40)), passed_map_corner=3)
+            self.chunks.append(chunk)
+
+    def move_player(self):
+        while True:
+            player_input = ' '
+            while player_input not in 'wasdWASD':
+                player_input = input('\u001b[33mType wasd to move: ')
+            self.chunks[0].map[self.player.pos[0]][self.player.pos[1]
+                                                   ].colliding_objects = None
+            if player_input in 'wW':
+                self.player.pos = (self.player.pos[0] - 1, self.player.pos[1])
+            if player_input in 'dD':
+                self.player.pos = (self.player.pos[0], self.player.pos[1] + 1)
+            if player_input in 'aA':
+                self.player.pos = (self.player.pos[0], self.player.pos[1] - 1)
+            if player_input in 'sS':
+                self.player.pos = (self.player.pos[0] + 1, self.player.pos[1])
+            self.chunks[0].map[self.player.pos[0]][self.player.pos[1]
+                                                   ].colliding_objects = [self.player]
+            print(chr(27) + "[2J")
+            print(self.chunks[self.current_chunk])
 
 
 class TileType(Enum):
     no_tile = '!'
-    rock = '@'
+    rock = '\u001b[37m@'
     barren = ' '
-    prairie = '-'
-    lush_prairie = '*'
-    forest = 'T'
+    prairie = '\u001b[34m-'
+    lush_prairie = '\u001b[36m*'
+    forest = '\u001b[32mT'
 
 
 POSIBLE_TILES = [TileType.rock, TileType.barren,
                  TileType.prairie, TileType.lush_prairie, TileType.forest]
 
-BASIC_PROBS = {
+BASIC_TILE_PROBS = {
     TileType.no_tile: [0., 0., 0., 0., 0.],
     TileType.rock: [0.005, 0.40, 0.595, 0., 0.],
     TileType.barren: [0.005, 0.905, 0.09, 0., 0.],
@@ -29,20 +70,38 @@ BASIC_PROBS = {
     TileType.forest: [0.005, 0., 0., 0.095, 0.9],
 }
 
+BASIC_FOODPROBS = {
+    TileType.rock: [0.75, 0.25, 0., 0.],
+    TileType.barren: [1., 0., 0., 0.],
+    TileType.prairie: [0.5, 0.1, 0.25, 0.15],
+    TileType.lush_prairie: [0.25, 0.1, 0.40, 0.25],
+    TileType.forest: [0.4, 0.1, 0.2, 0.3],
+}
+
+BASIC_FOOD = [None, 'harmful', 'normal', 'helpful']
+
 
 class Location:
-    def __init__(self, tile_type=None, non_colliding_objects=None, colliding_objects=None):
+    def __init__(self, tile_type=None, non_colliding_objects=None, colliding_objects=None, food_probabilities=BASIC_FOODPROBS, food_types=BASIC_FOOD):
         # Tile type, enum perhaps?
         self.tile = tile_type
         # A list of objects allows for stacking of food / poop potentially
         self.non_colliding_objects = non_colliding_objects
         # An oject too big to move through, Rocks, Players, Enemies
         self.colliding_objects = colliding_objects
+        self.food_probabilities = food_probabilities
+        self.food_types = food_types
 
-    def will_collision_occur(self):
+    def grow_food(self):
+        food = random.choices(
+            self.food_types, weights=self.food_probabilities[self.tile])[0]
+        if food is not None:
+            self.non_colliding_objects = [Food('allthesame', food)]
+
+    def will_collision_occur(self) -> bool:
         return self.colliding_objects is not None
 
-    def to_json(self):
+    def to_json(self) -> str:
         return json.dumps({
             'tile': str(self.tile),
             'non_c_o': self.non_colliding_objects,
@@ -93,7 +152,7 @@ class Chunk:
         self,
         random_seed,
         id,
-        probs=BASIC_PROBS,
+        probs=BASIC_TILE_PROBS,
         possible_tiles=POSIBLE_TILES,
         shape=(20, 40),
         d_con=(1, 1),
@@ -135,23 +194,16 @@ class Chunk:
                         for in_i in range(i - self.distance_considered[0], i + self.distance_considered[0] + 1):
                             for in_j in range(j - self.distance_considered[1], j + self.distance_considered[1] + 1):
                                 if in_i >= 0 and in_i < self.shape[0] and in_j >= 0 and in_j < self.shape[1]:
-                                    # print(f'i: {i} in_i: {in_i} j: {j} in_j: {in_j}')
                                     if self.map[in_i][in_j].tile != TileType.no_tile:
                                         sum_probs = [
                                             sum_probs[b] + self.tile_probabilities[self.map[in_i][in_j].tile][b] for b in range(len(sum_probs))]
                                         count_added_probs += 1
                         count_added_probs = 1 if count_added_probs == 0 else count_added_probs
-                        # print(sum_probs)
                         actual_probs = [
                             num/count_added_probs for num in sum_probs]
-                        # print(actual_probs)
-                        # print(sum(actual_probs))
-                        # print(actual_probs)
-                        # print(self.possible_tiles)
                         self.map[i][j].tile = random.choices(
                             self.possible_tiles, weights=actual_probs)[0]
-                # print('-----------')
-            # print('-----------*******-----------')
+                        self.map[i][j].grow_food()
 
     def place_passed_map_if_exists(self) -> None:
         if self.passed_map is not None:
@@ -196,18 +248,18 @@ class Chunk:
                 str_row += str(i)
             str_row += '\n'
             entire_map += str_row
-        return entire_map
+        return entire_map[:len(entire_map)-2]
 
     def slice(self, shape) -> list:
         return [self.map[i][shape[2]: shape[3]] for i in range(shape[0], shape[1])]
-        # return self.map[shape[0]:shape[2], shape[1]: shape[3]]
 
-    def save(self, folder='chunks/') -> str:  # file path
+    def save(self, folder='chunks/') -> str:
         path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                             folder)
         json_of_chunk = json.dumps({
             'id': self.id,
             'seed': self.random_seed,
+            'shape': self.shape,
             'chunk': [[l.to_json() for l in row] for row in self.map],
         })
         if not os.path.exists(path):
@@ -224,18 +276,23 @@ class Chunk:
                 [Location.from_json(json.loads(l)) for l in row] for row in chunk_json['chunk']]
             return cls(chunk_json['seed'],
                        chunk_json['id'],
+                       shape=chunk_json['shape'],
                        passed_map=chunk_json['chunk'],
                        passed_map_corner=1)
 
 
 if __name__ == '__main__':
-    chunk = Chunk(1, '0')
+    """
+    game_map = GameMap()
+    game_map.move_player()
+"""
+    chunk = Chunk(1, '0', shape=(100, 1000))
     print(chunk)
 
-    file_path = chunk.save()
-    loaded_chunk = Chunk.load_chunk_from_filepath(file_path)
-    print(loaded_chunk)
-    """
+    # file_path = chunk.save()
+    # loaded_chunk = Chunk.load_chunk_from_filepath(file_path)
+    # print(loaded_chunk)
+"""
     for i in range(1, 100):
         time.sleep(.200)
         chunk = Chunk(
