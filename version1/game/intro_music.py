7977727220b625c5a610917be5d185b5c43fff41
xScoopy <JLeary070@gmail.com>
2021-03-18 17:41:21 -0500

    fixed merge conflicts from importing upstream

--- version1/game/intro_music.py
+++ version1/game/intro_music.py
@@ -6,7 +6,6 @@ import pyglet as py
 # set audio driver options
 py.options['audio'] = ('openal', 'pulse', 'directsound', 'silent')
 
-<<<<<<< HEAD
 
 if __name__ == '__main__':
     nature = py.media.load("./resources/sounds/nature_background.wav", streaming=False)
@@ -29,20 +28,3 @@ if __name__ == '__main__':
     #works as is if run directly from this file
 
 
-=======
-
-# reference song files and load them
-intro = py.media.load("./resources/sounds/intro.wav")
-nature = py.media.load("./resources/nature_background.wav")
-# play intro
-background_player = py.media.Player()
-background_player.eos_action = py.media.SourceGroup.loop
-background_player.queue(nature)
-background_player.eos_action = py.media.SourceGroup.loop
-# intro.play()
-# nature.play()
-background_player.play()
-py.app.run()
-
-# works as is if run directly from this file
->>>>>>> d9a2f49b28959173aa110db636adb6ebf0354eaf
