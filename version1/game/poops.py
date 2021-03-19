43624c211659eaccca5269aa8ba48c4eb28c5479
xScoopy <JLeary070@gmail.com>
2021-03-19 11:22:59 -0500

    added a max poop value, added a helper to keep poops under max value, and added a get stats method to poops class

--- version1/game/poops.py
+++ version1/game/poops.py
@@ -1,19 +1,28 @@
 class Poops:
+    self.max_poops = 200
     def __init__(self, amount=5):
         # start the game with 5 poops, unless otherwise specified
         self.amount = amount
 
     def get_poops(self):
         return self.amount
+    
+    def max_poop_check(self):
+        if self.amount > self.max_poops:
+            self.amount = self.max_poops
 
     def poopsAvailable(self):
         return self.amount > 0
 
     def addPoops(self, amount):
         self.amount += amount
+        self.max_poop_check()
 
     def make_poop(self, x, y):
         self.amount -= 1
+    
+    def get_stats(self):
+        return f'{self.amount} / {self.max_poops}'
 
     def __repr__(self):
         return f'Poops:{self.amount}'
