d80ac9430ee65cce7e5d8dfac5a022aa80ba4a75
JohnminerIv <john.douglas.miner.iv@gmail.com>
2021-03-18 17:43:31 -0800

    Fixed movement input to be [x, y] so now we can do [0,1] to move up

--- version1/game_map/tile_type.py
+++ version1/game_map/tile_type.py
@@ -3,8 +3,13 @@ from enum import Enum
 
 class TileType(Enum):
     no_tile = '!'
-    rock = '\u001b[37m@'
-    barren = '\u001b[30m|'
-    prairie = '\u001b[34m-'
-    lush_prairie = '\u001b[36m*'
-    forest = '\u001b[32m^'
+    # \u001b[37m
+    rock = '@'
+    # \u001b[30m
+    barren = '|'
+    # \u001b[34m
+    prairie = '-'
+    # \u001b[36m
+    lush_prairie = '*'
+    # \u001b[32m
+    forest = '^'
