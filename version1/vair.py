3da9ea4162d4420ceb80d34a11c55473082466dd
Genji-MS <Genji.Tapia@students.makeschool.com>
2021-03-20 18:28:24 -0700

    scenes will not work as multiple scripts title & game_over refactored into vair.py

--- version1/vair.py
+++ version1/vair.py
@@ -1,97 +1,128 @@
 import pyglet as py
 import sprites
+from pyglet.window import mouse
 
-# set image path and specific sprites to files
+
+#////////////////////INIT//////////////////
 py.resource.path = ['../resources']
 py.resource.reindex()
 
+
+# Set audio driver priority
+py.options['audio'] = ('openal', 'pulse', 'directsound', 'silent')
+sound_player = py.media.Player()
 
 def anchor_center(image):
     """set the anchor point of an image to its center"""
     image.anchor_x = image.width//2
     image.anchor_y = image.height//2
 
-
-# instantiate the title rabbit animation class
-vair = sprites.Title_Rabbit()
-bun = sprites.Sprite_Rabbit()
-fox = sprites.Sprite_Fox()
-text = sprites.Title_Text()
-# re-center images
-for image in vair.title_seq:
-    anchor_center(image)
-for image in bun.img_seq:
-    anchor_center(image)
-for image in fox.img_seq:
-    anchor_center(image)
-for image in text.title_seq:
-    anchor_center(image)
-
-# create game window
 window = py.window.Window()
-batch = py.graphics.Batch()
-bg_color = py.shapes.Rectangle(
-    0, 0, window.width, window.height, color=(22, 78, 22))
+bg_color = py.shapes.Rectangle(0, 0, window.width, window.height, color=(22, 78, 22))
+vair = None
+text = None
+butn = None
+butn2 = None
+GAME_MODE = 'intro'
 
+def game_intro():
+    global GAME_MODE, sound_player, vair, text, butn, butn2
+    GAME_MODE = 'intro'
+    #////////////////////INTRO///////////////
+    song = py.media.load("../resources/sounds/intro_track.wav", streaming=False)
+    sound_player.queue(song)
 
-# create some text labels
-BUFFER = 8  # padding
-stats_border = py.shapes.Rectangle((BUFFER//2)-1, window.height - (
-    75+1) - (BUFFER//2), 120+2, 74+2, color=(255, 255, 255), batch=batch)
-stats_fill = py.shapes.Rectangle(
-    BUFFER//2, window.height - 75 - (BUFFER//2), 120, 74, color=(0, 0, 0), batch=batch)
-health_txt = py.text.Label(
-    'Thlay', x=BUFFER, y=window.height - 20 - BUFFER, batch=batch)
-stomach_txt = py.text.Label(
-    'Flay', x=BUFFER, y=window.height - 40 - BUFFER, batch=batch)
-poops_txt = py.text.Label(
-    'Hraka', x=BUFFER, y=window.height - 60 - BUFFER, batch=batch)
-#title_txt = py.text.Label('air', font_size = 78, x = vair.sprite.x + 10, y=vair.sprite.y + 40)
+    #////////////////////////////////////////    
+    vair = sprites.Title_Rabbit()
+    text = sprites.Title_Text()
+    butn = py.resource.image("title/start_button.png")
+    butn.width, butn.height = 100, 50
+    anchor_center(butn)
+    for image in vair.title_seq:
+        anchor_center(image)
+    for image in text.title_seq:
+        anchor_center(image)
+    #/////////////UNSCHEDULING///////////////
+    clocks = [vair, text]
+    for clock in clocks:
+        py.clock.unschedule(clock)
+    # SCHEDULE intro 
+    py.clock.schedule_interval(vair.update, 0.07)
+    py.clock.schedule_interval(text.update, 0.1)
 
-# calls our animation instance to update every 0.1 seconds
-py.clock.schedule_interval(vair.update, 0.1)
-py.clock.schedule_interval(bun.update, 0.07)
-py.clock.schedule_interval(fox.update, 0.1)
-py.clock.schedule_interval(text.update, 0.1)
+def game_outro():
+    global GAME_MODE, sound_player, vair, text, butn, butn2
+    GAME_MODE = 'outro'
+    #//////////////////OUTRO/////////////////
+    song = py.media.load("../resources/sounds/game_over.wav", streaming=False)
+    sound_player.queue(song)
 
-# event handeler
+    vair = sprites.Dead_Rabbit()
+    text = None
+    butn = py.resource.image("title/NewMap.png")
+    butn2 = py.resource.image("title/SameMap.png")
+    butn.width, butn.height = 100, 50
+    butn2.width, butn2.height = 100, 50
+    anchor_center(butn)
+    anchor_center(butn2)
+    for image in vair.img_seq:
+        anchor_center(image)
+    #/////////////UNSCHEDULING///////////////
+    clocks = [vair, text]
+    for clock in clocks:
+        py.clock.unschedule(clock)
+    # SCHEDULE outro
+    py.clock.schedule_interval(vair.update, 0.07)
 
+game_intro()
+#////////////////////////////////////////
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    global GAME_MODE
+    if button == mouse.LEFT:
+        if GAME_MODE == 'intro':
+            #Intro
+            if x > 275 and x < 375:
+                if y > 25 and y < 75:
+                    print("you clicked the start button")
+                    game_outro()
+                    # insert game transition here
+                    # discard player() object playing the song here
+        if GAME_MODE == 'outro':
+            #Outro
+            if x > 175 and x < 275:
+                if y > 25 and y < 75:
+                    print("New Map")
+                    game_intro()
+            elif x > 375 and x < 475:
+                if y > 25 and y < 75:
+                    print("Same Map")
+                # insert game transition here
+                # discard player() object playing the song here
 
 @window.event
 def on_draw():
-    window.clear()
-    bg_color.draw()
-    vair.sprite.draw()
-    bun.sprite.draw()
-    fox.sprite.draw()
-    batch.draw()
-    text.sprite.draw()
-    # title_txt.draw()
+    global GAME_MODE
+    if GAME_MODE == 'intro':
+        #///////INTRO///////
+        window.clear()
+        bg_color.draw()
+        vair.sprite.draw()
+        text.sprite.draw()
+        butn.blit(325, 55)
+        sound_player.play()
+    elif GAME_MODE == 'outro':
+        #//////OUTRO////////
+        window.clear()
+        bg_color.draw()
+        vair.sprite.draw()
+        butn.blit(225, 55)
+        butn2.blit(425, 55)
+        sound_player.play()
 
-# forces a redraw, will create flicker if used excessivly
-# window.flip()
-
-
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == py.window.key.SPACE:
-        bun.nom()
-    elif symbol == py.window.key.Q:
-        bun.hop_out('L')
-        # bun.x += 10
-        # bun.y += 10
-    elif symbol == py.window.key.A:
-        bun.hop_in('L')
-    elif symbol == py.window.key.E:
-        bun.hop_out('R')
-    elif symbol == py.window.key.D:
-        bun.hop_in('R')
-    elif symbol == py.window.key.W:
-        bun.hop_out()
-    elif symbol == py.window.key.S:
-        bun.hop_in()
-
+#////////////////////////////////////////
 
 if __name__ == '__main__':
     # creates the event loop
     py.app.run()
+
