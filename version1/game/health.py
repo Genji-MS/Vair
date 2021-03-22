8664ea895e56393d83c7f92fb86b0ebe878ee518
xScoopy <JLeary070@gmail.com>
2021-03-22 17:09:14 -0500

    moved display proportion to individual classes and out of the game file

--- version1/game/health.py
+++ version1/game/health.py
@@ -32,7 +32,7 @@ class Health:
             color = (128, 128, 0)
         elif bar_percent <= 0.25:
             color = (255, 0, 0)
-        return [bar_percent, color]
+        return [bar_percent * 50, color]
     
     def new_game(self):
         self.current_hp = self.max_hp
